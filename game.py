f7cd06d200eea98d941d38329aecfb1e7407158e
Rodrigo Trindade <roelisontrindade@outlook.com>
2021-05-23 02:53:33 -0300

    window and main loop defined

--- game.py
+++ game.py
@@ -1,2 +1,13 @@
 import turtle
 
+#window setup
+window = turtle.Screen()
+window.title("Classic Pong")
+window.bgcolor("Black")
+window.setup(width=800, height=600)
+window.tracer(0)
+
+#Main game loop
+while True:
+    window.update()
+    
