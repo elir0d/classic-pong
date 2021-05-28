7feeb49085d424db8023a5dc7341743855e60dc3
Rodrigo Trindade <roelisontrindade@outlook.com>
2021-05-27 23:45:46 -0300

    revert(feature): :rewind:

--- modules/window_menu.py
+++ modules/window_menu.py
@@ -1,14 +1,7 @@
-import turtle
 import time
-
+import turtle
+import sys
 window = turtle.Screen()
-window.bgcolor("Black")
-window.setup(width=800, height=600)
-window.tracer(0)
-window.goto(0, 0)
-window.listen()
-
-
 
 def exit_pong():
     window.bye()
