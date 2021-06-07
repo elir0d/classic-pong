6e4b7ae45534290426f09029779a3e1d7397e212
Rodrigo Trindade <roelisontrindade@outlook.com>
2021-06-06 21:28:32 -0300

    feat: :sparkles: Include Two balls in the game
    Now it is possible to play with two balls.  Became more fun!

--- modules/ball_element.py
+++ modules/ball_element.py
@@ -1,12 +1,28 @@
 import turtle
 
-#----------ball ball----------#
+#----------balls----------#
 
-ball = turtle.Turtle()
-ball.speed(0)
-ball.shape("square")
-ball.color("white")
-ball.penup()
-ball.goto(0, 0)
+# Ball One
+ball1 = turtle.Turtle()
+ball1.speed(0)
+ball1.shape("square")
+ball1.color("blue")
+ball1.penup()
+ball1.goto(0, 0)
+ball1.dx = 2
+ball1.dy = 2
 
-#----------ball ball----------#
+# Ball Two
+ball2 = turtle.Turtle()
+ball2.speed(0)
+ball2.shape("square")
+ball2.color("red")
+ball2.penup()
+ball2.goto(0, 0)
+ball2.dx = -2
+ball2.dy = -2
+
+# deifne a list that can be responsible for trackin each ball.
+balls = [ball1, ball2] 
+
+#----------balls----------#
